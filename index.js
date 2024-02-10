79140263bc7c0226b5e2206ce70dc2f1418cee5b
sonny faristrianto <sonnyf4r15@gmail.com>
2024-02-10 13:51:19 +0700

    tambah file index

---
+++ index.js
@@ -0,0 +1,10 @@
+var a = "No";
+var b = "rumah saya";
+var c = 30;
+var d = "A";
+var e = "Jalan pangeran Tirtayasa No";
+var f = "70";
+
+soal1 = a +' '+ b +' '+ String(c) +d;
+console.log(soal1);
+console.log(soal1);
